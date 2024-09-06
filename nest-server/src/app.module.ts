14d9d57da15a2c40bcecc982c7b02892f47f0d72
Maxim Yakovlev <it.trainee@yandex.ru>
2024-09-06 11:16:43 +0300

    imported Sequelize module

--- nest-server/src/app.module.ts
+++ nest-server/src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 3306,
+      username: 'postgres_user',
+      password: 'postgres_password',
+      database: 'postgres_db',
+      models: [],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
