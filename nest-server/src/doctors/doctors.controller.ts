651486590ac7f49f4fc97488ea9c99dff688a1eb
Maxim Yakovlev <it.trainee@yandex.ru>
2024-09-08 14:50:40 +0300

    implemented the doctor's controller

--- nest-server/src/doctors/doctors.controller.ts
+++ nest-server/src/doctors/doctors.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Body, Delete, Param, Post } from '@nestjs/common';
+import { DoctorsService } from './doctors.service';
+import { CreateDoctorDto } from './dto/CreateDoctorDto';
+import { Doctor } from './models/doctor.model';
 
 @Controller('doctors')
-export class DoctorsController {}
+export class DoctorsController {
+  constructor(private readonly doctorsService: DoctorsService) {}
+
+  @Post()
+  async create(@Body() createDoctorDto: CreateDoctorDto): Promise<Doctor> {
+    return this.doctorsService.create(createDoctorDto);
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: number): Promise<void> {
+    return this.doctorsService.remove(id);
+  }
+}
--- nest-server/src/doctors/doctors.module.ts
+++ nest-server/src/doctors/doctors.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { DoctorsService } from './doctors.service';
+import { DoctorsController } from './doctors.controller';
 import { Doctor } from './models/doctor.model';
 import { DoctorPatient } from './models/doctor-patient.model';
 
 @Module({
   imports: [SequelizeModule.forFeature([Doctor, DoctorPatient])],
-  controllers: [],
-  providers: [],
-  exports: [SequelizeModule],
+  controllers: [DoctorsController],
+  providers: [DoctorsService],
+  exports: [SequelizeModule, DoctorsService],
 })
 export class DoctorsModule {}
