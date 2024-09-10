25385123779aeee687231a72f8edaedf07530370
Maxim Yakovlev <it.trainee@yandex.ru>
2024-09-10 10:00:51 +0300

    added: 'AdminService' class method

--- nest-server/src/admin/admin.service.ts
+++ nest-server/src/admin/admin.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { Admin } from './models/admin.model';
 
 @Injectable()
-export class AdminService {}
+export class AdminService {
+  private readonly admins: Admin[];
+
+  async findOne(username: string): Promise<Admin | undefined> {
+    return this.admins.find((admin) => admin.username === username);
+  }
+}
