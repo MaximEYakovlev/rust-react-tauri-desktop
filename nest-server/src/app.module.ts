73edafefcebffda89a40218979a10052140fe12a
Maxim Yakovlev <it.trainee@yandex.ru>
2024-09-10 09:15:23 +0300

    added: auth module, installed dependencies: passport-local, passport-jwt

--- nest-server/src/app.module.ts
+++ nest-server/src/app.module.ts
@@ -3,9 +3,10 @@ import { DbModule } from './db/db.module';
 import { AdminModule } from './admin/admin.module';
 import { DoctorsModule } from './doctors/doctors.module';
 import { PatientsModule } from './patients/patients.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [DbModule, AdminModule, DoctorsModule, PatientsModule],
+  imports: [DbModule, AdminModule, DoctorsModule, PatientsModule, AuthModule],
   controllers: [],
   providers: [],
 })
