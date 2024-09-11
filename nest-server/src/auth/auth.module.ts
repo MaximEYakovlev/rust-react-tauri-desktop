f9bae24e002223285190668998cc1bbd3d0de914
Maxim Yakovlev <it.trainee@yandex.ru>
2024-09-11 09:40:00 +0300

    updated AuthModule

--- nest-server/src/auth/auth.module.ts
+++ nest-server/src/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AdminModule } from 'src/admin/admin.module';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './local.strategy';
 
 @Module({
-  imports: [AdminModule],
-  providers: [AuthService],
+  imports: [AdminModule, PassportModule],
+  providers: [AuthService, LocalStrategy],
 })
 export class AuthModule {}
