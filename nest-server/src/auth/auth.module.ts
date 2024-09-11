4041ae00d2c1940b6f238c47662394a77a041cfb
Maxim Yakovlev <it.trainee@yandex.ru>
2024-09-11 09:49:54 +0300

    added: auth controller

--- nest-server/src/auth/auth.module.ts
+++ nest-server/src/auth/auth.module.ts
@@ -3,9 +3,11 @@ import { AuthService } from './auth.service';
 import { AdminModule } from 'src/admin/admin.module';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './local.strategy';
+import { AuthController } from './auth.controller';
 
 @Module({
   imports: [AdminModule, PassportModule],
   providers: [AuthService, LocalStrategy],
+  controllers: [AuthController],
 })
 export class AuthModule {}
