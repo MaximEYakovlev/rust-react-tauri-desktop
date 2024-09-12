dbc58f617aa686d4487391b10adab86148dfb742
Maxim Yakovlev <it.trainee@yandex.ru>
2024-09-12 10:35:06 +0300

    updated auth module

--- nest-server/src/auth/auth.module.ts
+++ nest-server/src/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { AuthService } from './auth.service';
 import { AdminModule } from 'src/admin/admin.module';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './local.strategy';
+import { JwtStrategy } from './jwt.strategy';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from './constants';
@@ -16,7 +17,7 @@ import { jwtConstants } from './constants';
       signOptions: { expiresIn: '60s' },
     }),
   ],
-  providers: [AuthService, LocalStrategy],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
   controllers: [AuthController],
   exports: [AuthService],
 })
