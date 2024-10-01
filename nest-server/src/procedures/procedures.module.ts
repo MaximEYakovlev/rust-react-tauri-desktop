2dd146235f2241341a4fb3b90289418010b49280
Maxim Yakovlev <it.trainee@yandex.ru>
2024-10-01 17:30:30 +0300

    updated 'procedures', 'treatments' modules

--- nest-server/src/procedures/procedures.module.ts
+++ nest-server/src/procedures/procedures.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Procedure } from './models/procedure.model';
 
-@Module({})
+@Module({
+  imports: [SequelizeModule.forFeature([Procedure])],
+  controllers: [],
+  providers: [],
+  exports: [SequelizeModule],
+})
 export class ProceduresModule {}
--- nest-server/src/treatments/treatments.module.ts
+++ nest-server/src/treatments/treatments.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Treatment } from './models/treatment.model';
 
-@Module({})
+@Module({
+  imports: [SequelizeModule.forFeature([Treatment])],
+  controllers: [],
+  providers: [],
+  exports: [SequelizeModule],
+})
 export class TreatmentsModule {}
