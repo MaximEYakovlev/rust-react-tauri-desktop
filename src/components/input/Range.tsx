785b3183fc742e5f3a0d7e93db46d2ff5d166f18
Maxim Yakovlev <maxim.e.yakovlev@gmail.com>
2025-01-29 17:33:19 +0300

    added src/assets/svg directory; converted the 'Range.jsx' into 'Range.tsx'

--- src/components/input/Range.tsx
+++ src/components/input/Range.tsx
@@ -1,12 +1,13 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import styles from './Range.module.css'
 
-export const Range = () => {
+
+export const Range: React.FC = () => {
     const [value, setValue] = useState(50);
     let minValue = 0;
     let maxValue = 100;
 
-    const handleChange = (delta) => {
+    const handleChange = (delta: number) => {
         let newValue = value - delta;
 
         if (newValue >= minValue && newValue <= maxValue) {
@@ -17,11 +18,11 @@ export const Range = () => {
     return (
         <div className={styles.container}>
             <button onClick={() => handleChange(4)}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"><path d="M7.293 4.707 14.586 12l-7.293 7.293 1.414 1.414L17.414 12 8.707 3.293 7.293 4.707z" /></svg>
+                <img src="" alt="" />
             </button>
             <input type="range" min={minValue} max={maxValue} value={value} step={4} />
             <button onClick={() => handleChange(-4)}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"><path d="M7.293 4.707 14.586 12l-7.293 7.293 1.414 1.414L17.414 12 8.707 3.293 7.293 4.707z" /></svg>
+                <img src="" alt="" />
             </button>
         </div>
     )
