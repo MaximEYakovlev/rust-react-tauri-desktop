cc8273b054205f29e738d0bed9b5a1b52903aa0e
Maxim Yakovlev <maxim.e.yakovlev@gmail.com>
2025-01-29 16:34:16 +0300

    added svg icons to buttons

--- src/components/input/Range.jsx
+++ src/components/input/Range.jsx
@@ -16,9 +16,13 @@ export const Range = () => {
 
     return (
         <div className={styles.container}>
-            <button onClick={() => handleChange(4)}>+</button>
+            <button onClick={() => handleChange(4)}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"><path d="M7.293 4.707 14.586 12l-7.293 7.293 1.414 1.414L17.414 12 8.707 3.293 7.293 4.707z" /></svg>
+            </button>
             <input type="range" min={minValue} max={maxValue} value={value} step={4} />
-            <button onClick={() => handleChange(-4)}>-</button>
+            <button onClick={() => handleChange(-4)}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"><path d="M7.293 4.707 14.586 12l-7.293 7.293 1.414 1.414L17.414 12 8.707 3.293 7.293 4.707z" /></svg>
+            </button>
         </div>
     )
 }
